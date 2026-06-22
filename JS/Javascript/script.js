7980bdcd75651533b503d0a125683c3c9f272bdf
Nitin Bhardwaj <bhardwajnitin9548@gmail.com>
2026-06-22 19:39:54 +0530

    Using different elements in Jaavascript from HTML

---
+++ JS/Javascript/script.js
@@ -0,0 +1,16 @@
+// console.log(`Hello World!`);
+// window.alert("Welcome");
+// document.getElementById("para").innerHTML="<b><u>This is the power of Javascript</u></b>";
+// document.getElementById("heading").textContent="Javascript and React";
+
+
+let name="Rahul";
+let age=40;
+let isStudent=true;
+
+document.getElementById("para1").innerHTML=(`My name is ${name} and I am ${age} years old, 
+Student:${isStudent}`);
+
+// document.getElementById("para1").textContent=name;
+// document.getElementById("para2").textContent=age;
+// document.getElementById("para3").textContent=isStudent;
