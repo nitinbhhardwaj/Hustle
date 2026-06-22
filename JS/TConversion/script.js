e54fb0a1fd31d78acb3436b66650ff557c6ae0f0
Nitin Bhardwaj <bhardwajnitin9548@gmail.com>
2026-06-22 23:06:28 +0530

    Typecasting

---
+++ JS/TConversion/script.js
@@ -0,0 +1,16 @@
+// let age=window.prompt("What is your age?");
+// age=Number(age);
+// age+=1;
+// document.getElementById("head").innerHTML=`${age}`;
+
+
+let x="pizza";
+let y="pizza";
+let z="pizza";
+
+y=Number(y);
+z=Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
