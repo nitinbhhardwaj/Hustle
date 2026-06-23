0b2d9f142ef6d5fec9f8bd10eac18e123af4c1dd
Nitin Bhardwaj <bhardwajnitin9548@gmail.com>
2026-06-23 08:58:28 +0530

    Whether a number is armstrong number or not

--- JS/Conditionals/script.js
+++ JS/Conditionals/script.js
@@ -1,14 +1,28 @@
-document.getElementById("submit").onclick=function(){
-    let age=document.getElementById("age").value;
-    age=Number(age);
-    if(age>=18){
-        document.getElementById("head").textContent=`Eligible to vote`;
-    }
-    else{
-        document.getElementById("head").textContent=`Not Eligible to vote`;
-    }
+// document.getElementById("submit").onclick=function(){
+//     let age=document.getElementById("age").value;
+//     age=Number(age);
+//     if(age>=18){
+//         document.getElementById("head").textContent=`Eligible to vote`;
+//     }
+//     else{
+//         document.getElementById("head").textContent=`Not Eligible to vote`;
+//     }
     
-}
+// }
 
+let n=window.prompt("Enter the number?");
+let flag=true;
+for(let i=1;i<=n/2;i++){
+    if(n%i==0){
+        flag=false;
+        break;
+    }
+}
+if(flag==true){
+    document.getElementById("head").textContent="Prime Number";
+}
+else{
+    document.getElementById("head").textContent="Composite Number";
+}
 
 
